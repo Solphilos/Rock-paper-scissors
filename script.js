0c744bb8f503999e71511eacfdd15b38cd9b7e60
solphilos <trichosirius@gmail.com>
2022-03-03 16:41:35 -0500

    game fully functional

--- script.js
+++ script.js
@@ -6,13 +6,40 @@ function computerPlay() {
     return hand;
 }
 
-function singleRound(playerSelection, computerSelection) {
+function singleRound() {
     
-    let playerSelection = prompt('Rock, paper or scissors?');
+    let playerChoice = prompt('Rock, paper or scissors?');
     alert(`you chose ${playerChoice}`);
-    let computerSelection = computerPlay();
+    let computerChoice = computerPlay();
     alert(`computer chooses ${computerChoice}`);
 
- 
+  if (playerChoice === 'rock' && computerChoice === 'scissors') {
+      alert('rock crushes scissors, you win!')
+  }
+  else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+      alert('scissors cut paper, you lose!')
+  }
+  else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+      alert('scissors cut paper, you win!') 
+  }
+  else if (playerChoice === 'rock' && computerChoice === 'paper') {
+      alert('paper...erm....covers rock...computer wins, I guess');
+  }
+  else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+      alert('rock smashes scissors, you lose!');
+  }
+  else if (playerChoice === 'paper' && computerChoice === 'rock') {
+      alert('paper...erm...covers rock? You win, I guess');
+  }
+  else {
+      alert('its a tie, play again');
+  }
 }
- 
+
+function game() {
+    for (i = 0; i < 5; i ++) {
+        singleRound();
+    }
+}
+
+game()
